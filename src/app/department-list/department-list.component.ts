c83969b0fc7cea959b3ddc9de3f1e34b548b55d4
meetmehta26 <meet7b@gmail.com>
2021-02-10 13:23:09 +0530

    data modificatin from employee to department

--- src/app/department-list/department-list.component.ts
+++ src/app/department-list/department-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-department-list',
@@ -6,15 +8,42 @@ import { Component, OnInit } from '@angular/core';
     <p>
       department-list works!
     </p>
+    {{errormessage}}
+    <h3>
+    Department List
+  </h3>
+  <ul class="items">
+    <li *ngFor="let department of department" (click)="onSelect(department)">
+      <span class="badge">{{department.id}}</span> {{department.name}}
+    </li>
+  </ul>
+  
   `,
   styles: [
   ]
 })
 export class DepartmentListComponent implements OnInit {
-
-  constructor() { }
+  public errormessage =''; 
+  public departmentList: any[]=[]; 
+  department :any[]= [
+    {"id": 1, "name": "Angular"},
+    {"id": 2, "name": "Node"},
+    {"id": 3, "name": "MongoDB"},
+    {"id": 4, "name": "Ruby"},
+    {"id": 5, "name": "Bootstrap"}
+  ]
+  constructor(private employeeeService : EmployeeService, private router: Router) { }
 
   ngOnInit(): void {
+    this.employeeeService.getEmployee()
+      .subscribe(data =>this.departmentList=data ,
+        error => this.errormessage=error
+        )
+
+  }
+  onSelect(department: { id: any; }){
+    this.router.navigate(['/department',department.id])
+
   }
 
 }
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -6,15 +6,6 @@ import { Router} from '@angular/router';
   selector: 'app-employee-list',
   template: `
   <h2> Employee List component</h2>
-  {{errormessage}}
-  <h3>
-  Department List
-</h3>
-<ul class="items">
-  <li *ngFor="let department of department" (click)="onSelect(department)">
-    <span class="badge">{{department.id}}</span> {{department.name}}
-  </li>
-</ul>
 `,
     
   
@@ -23,26 +14,13 @@ import { Router} from '@angular/router';
 })
 export class EmployeeListComponent implements OnInit {
   public employee :any[]= [];
-  public errormessage ='';
-  department :any[]= [
-    {"id": 1, "name": "Angular"},
-    {"id": 2, "name": "Node"},
-    {"id": 3, "name": "MongoDB"},
-    {"id": 4, "name": "Ruby"},
-    {"id": 5, "name": "Bootstrap"}
-  ]
+ 
+  
   constructor(private employeeeService : EmployeeService, private router: Router) { }
 
   ngOnInit(): void {
-     this.employeeeService.getEmployee()
-      .subscribe(data =>this.employee=data ,
-        error => this.errormessage=error
-        )
 
   }
-  onSelect(department: { id: any; }){
-    this.router.navigate(['/department',department.id])
-
-  }
+  
 
 }
