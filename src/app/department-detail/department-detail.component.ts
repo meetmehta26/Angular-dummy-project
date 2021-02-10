7ff2ac9b26af9e4573fd0fb5d0807b774a6010ce
meetmehta26 <meet7b@gmail.com>
2021-02-10 13:06:19 +0530

    paramMap observables added and pre and next button

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -4,16 +4,35 @@ import { Router,ActivatedRoute, ParamMap} from '@angular/router'
   selector: 'app-department-detail',
   template: `
   <h3>You selected department with id = {{departmentId}}</h3>
+  <nav>
+  <a (click) = onPrevious()><span class="badge">Previous</span></a>
+  &nbsp;
+  <a (click) = onNext()><span class="badge">Next</span></a>
+  </nav>
   `,
   styleUrls: []
 })
 export class DepartmentDetailComponent implements OnInit {
 public departmentId;
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute,private router : Router) { }
 
   ngOnInit(): void {
-    let id = parseInt(this.route.snapshot.paramMap.get('id')!);
-    this.departmentId=id;
+    // let id = parseInt(this.route.snapshot.paramMap.get('id')!);
+    // this.departmentId=id;
+    this.route.paramMap.subscribe((params:ParamMap) =>{
+        let id = parseInt(params.get('id')!);
+        this.departmentId=id;
+
+    })
+  }
+  onPrevious(){
+    let previous : number = this.departmentId-1;
+    this.router.navigate(['/department',previous]);
+
+  }
+  onNext(){
+    let next : number = this.departmentId+1;
+    this.router.navigate(['/department',next]);
   }
 
 }
